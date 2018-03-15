7711454bb9ec4d22f890f4d3e5f5427948a637f6
aajarven <anni.jarvenpaa@gmail.com>
2018-03-15 23:34:39 +0200

    use new functions

--- vuokraovi.py
+++ vuokraovi.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import re
+import html
 import requests
-from HTMLParser import HTMLParser
+import stringHandler
 import urllib
-
-class MyHTMLParser(HTMLParser):
-
-	def __init__(self, kuvat):
-		self.kuvat = kuvat
-		HTMLParser.__init__(self)
-	
-	def handle_startendtag(self, tag, attrs):
-		if tag == "img":
-			for a in attrs:
-				if a[0] == "src" or a[0] == "data-cycle-src":
-					if ("_original") in a[1]:
-						self.kuvat.append(a[1])
-
 
 #url = "https://www.vuokraovi.com/vuokra-asunto/helsinki/arabianranta/kerrostalo/547253"
 url = "https://www.vuokraovi.com/vuokra-asunto/helsinki/kerrostalo/169310"
@@ -26,23 +12,20 @@ url = "https://www.vuokraovi.com/vuokra-asunto/helsinki/kerrostalo/169310"
 #headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'}
 
 r = requests.get(url)
+htmlText = stringHandler.removeExcessNewlines(r.text)
 
 raakakuvat = []
-parser = MyHTMLParser(raakakuvat)
+parser = html.imageFinder(raakakuvat)
+parser.feed(htmlText)
 
-html = "\n".join([ll.rstrip() for ll in r.text.splitlines() if ll.strip()]) 
-
-parser.feed(html)
-kuvaurlit = []
-for kuva in raakakuvat:
-	if "140x50,fit" not in kuva:
-		k = re.sub(r"\d+x\d+,fit/", "", kuva)
-		kuvaurlit.append(k.replace("//", "https://", 1))
+kuvaurlit = stringHandler.pickImageUrls(raakakuvat)
 
 kuvaindeksi = 1
 for kuvaurl in kuvaurlit:
-	urllib.urlretrieve(kuvaurl, "testikuva"+str(kuvaindeksi))
+	urllib.urlretrieve(kuvaurl, stringHandler.generateFilename(kuvaurl,
+															"testikuva" +
+															'{:02d}'.format(kuvaindeksi)))
 	kuvaindeksi += 1
 
 f = open('output.txt', 'w')
-f.write(html.encode('utf8'))
+#f.write(html.encode('utf8'))
