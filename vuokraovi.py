92c41a603470fe35b041a499c98610c4b77b82f1
aajarven <anni.jarvenpaa@gmail.com>
2018-03-16 02:35:59 +0200

    fully working

--- vuokraovi.py
+++ vuokraovi.py
@@ -2,6 +2,8 @@
 
 import html
 from optparse import OptionParser
+import os
+import re
 import requests
 import stringHandler
 import urllib
@@ -18,30 +20,41 @@ if __name__ == '__main__':
 	if not dirname.endswith('/'):
 		dirname = dirname + '/'
 
+	# html
 	r = requests.get(url)
 	htmlText = stringHandler.removeExcessNewlines(r.text)
 
+	# apartment data
+	dataDict = {}
+	tableParser = html.tableReader(dataDict)
+	tableParser.feed(stringHandler.removeParagraphTags(htmlText))
+
+	# directory for saving
+	fullDirname = dirname + dataDict['Kohdenumero'] + "-" +	dataDict['Kuvaus'] + "-" + re.findall(r"\d+",
+									   dataDict['Asuinpinta-ala'])[0] + "/"
+	if not os.path.exists(fullDirname):
+		os.makedirs(fullDirname)
+
+	# write apartment information
+	f = open(fullDirname + "info.txt", 'w')
+	f.write(dataDict['description'].encode('utf8'))
+	f.write("\n")
+	for key in dataDict:
+		if key != 'description':
+			line = key + ": " + dataDict[key]
+			f.write("\n")
+			f.write(line.encode('utf-8'))
+	f.close()
+
+	# images
 	rawUrls = []
 	imageParser = html.imageFinder(rawUrls)
 	imageParser.feed(htmlText)
-
 	imageUrls = stringHandler.pickImageUrls(rawUrls)
 
 	imageIndex = 1
 	for imageUrl in imageUrls:
-		urllib.urlretrieve(imageUrl, stringHandler.generateFilename(imageUrl,
-																dirname +
-															 "testikuva" +
-																'{:02d}'.format(imageIndex)))
+		urllib.urlretrieve(imageUrl,
+					 stringHandler.generateFilename(imageUrl, fullDirname + "kuva"
+									 + '{:02d}'.format(imageIndex)))
 		imageIndex += 1
-
-	#asdf = stringHandler.removeParagraphTags(htmlText)
-	#f = open('output.txt', 'w')
-	#f.write(asdf.encode('utf8'))
-	
-	dataDict = {}
-	tableParser = html.tableReader(dataDict)
-	tableParser.feed(stringHandler.removeParagraphTags(htmlText))
-#	for key in dataDict:
-#		print(key + ": " + dataDict[key])
-
